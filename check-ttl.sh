4769a10452030b97f59c837ad6b6fbf90f166e75
Michal Pokorný <prvak.cnx+git@gmail.com>
2016-10-12 16:29:17 +0200

    Add check-ttl

--- check-ttl.sh
+++ check-ttl.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+JENA_DIR=~/downloads/apache-jena-3.1.0
+
 set -e
 
 cat structured/**/*.ttl > merged.ttl
 rm -rf /tmp/fusekidb
 mkdir -p /tmp/fusekidb
-~/downloads/apache-jena-3.1.0/bin/tdbloader2 --loc /tmp/fusekidb merged.ttl
+$JENA_DIR/bin/tdbloader2 --loc /tmp/fusekidb merged.ttl
 rm merged.ttl
